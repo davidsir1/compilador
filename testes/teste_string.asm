f4d6ff5e53bbf7c76579fdb3c328a74b027a99e4
Davi Lima <davidlima35566@gmail.com>
2026-09-21 23:52:53 -0300

    fix: correção da diretiva .asciiz

--- testes/teste_string.asm
+++ testes/teste_string.asm
@@ -1,7 +1,7 @@
 .data
     msg1: .asciiz "Ola mundo"
     msg2: .ascii  "Sem terminador"
-    msg3: .asciz  "Com escape \n e \t"
+    msg3: .asciiz  "Com escape \n e \t"
     msg4: .asciiz "Aspas: \" e barra: \\"
     msg5: .asciiz "Caractere nulo \0 no meio"
     msg6: .asciiz ""
